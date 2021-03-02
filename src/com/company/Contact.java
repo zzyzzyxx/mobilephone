b94cb45c2c891f680dbcff409250a76d33170aac
zzyzzyxx <molken24@gmail.com>
2021-03-02 17:10:03 +0000

    Initial commit

---
+++ src/com/company/Contact.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Contact {
+    private String name, number;
+   //
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public static Contact createContact(String name, String number){
+        return new Contact(name, number);
+    }
+}
+
